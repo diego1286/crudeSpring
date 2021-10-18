e982302c310ef7bb30bf65a4017b4112a2b5afc8
Diego Ardila <63212406+diego1286@users.noreply.github.com>
2021-10-18 15:49:13 -0500

    comentario

--- src/main/java/com/crudeClase/CrudeClase/CrudeClaseApplication.java
+++ src/main/java/com/crudeClase/CrudeClase/CrudeClaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+// COMENTARIO HECHO
 @SpringBootApplication
 public class CrudeClaseApplication implements CommandLineRunner {
 	@Autowired
